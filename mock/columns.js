ca4b9f3d9ebf4e6684ba3f6b93fe00e557e93b7f
Harold Cabalic <harold_cabalic@intuit.com>
2024-10-24 00:50:07 -0700

    Init table and data structure
    n/a

---
+++ mock/columns.js
@@ -0,0 +1,8 @@
+const COLUMNS = [
+    { field: 'isChecked', title: '' },
+    { field: 'name', title: 'Name' },
+    { field: 'product', title: 'Product' },
+    { field: 'created', title: 'Create Date' },
+    { field: 'modified', title: 'Modified Date' },
+    { field: 'owner', title: 'Owner' },
+]
