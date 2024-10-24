40ccef5e6c5694ec793734070aef644e32e77043
Harold Cabalic <harold_cabalic@intuit.com>
2024-10-24 11:49:23 -0700

    progress on filters

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,6 +2,7 @@ import { DataTable } from 'primereact/datatable'
 import { Checkbox } from 'primereact/checkbox'
 import { Column } from 'primereact/column'
 import { PROJECTS } from './data/mock'
+import { useEffect, useState } from 'react'
 
 const COLUMNS = [
     { field: 'isChecked', title: '', filter: '' },
@@ -14,6 +15,16 @@ const COLUMNS = [
 ]
 
 export default function Table() {
+    const [owners, setOwners] = useState([])
+    const [products, setProducts] = useState([])
+    const [keywordFilters, setKeywordFilters] = useState({
+        // TODO: set default value and filter rules
+    })
+
+    useEffect(() => {
+        setFilterLists(PROJECTS)
+    }, [])
+
     return (
         <DataTable
             value={PROJECTS}
@@ -23,14 +34,22 @@ export default function Table() {
             stripedRows
             showGridlines
             rowHover={true}
-            // filters={['contains']}
+            globalFilterFields={['product', 'owner']}
+            filters={['contains']}
             filterDisplay="row"
             id="foo"
             tableStyle={{ minWidth: '50rem' }}
         >
             <Column field="isSelected" body={checkboxTemplate}></Column>
             <Column field="name" header="Name" sortable filter></Column>
-            <Column field="product" header="Product" sortable filter></Column>
+            <Column
+                field="product"
+                header="Product"
+                sortable
+                filter
+                filterField="product"
+                showFilterMenu={false}
+            ></Column>
             <Column
                 field="created"
                 header="Created Date"
@@ -43,10 +62,42 @@ export default function Table() {
                 sortable
                 filter
             ></Column>
-            <Column field="owner" header="Owner" sortable filter></Column>
+            <Column
+                field="owner"
+                header="Owner"
+                sortable
+                filterField="owner"
+                filter
+                showFilterMenu={true}
+            ></Column>
             <Column field="actions" body={actionsTemplate}></Column>
         </DataTable>
     )
+
+    function setFilterLists(data) {
+        const owners = []
+        const products = []
+
+        for (let i = 0; i < data.length; ++i) {
+            if (owners.indexOf(data[i].owner) === -1) {
+                owners.push(data[i].owner)
+            }
+
+            if (products.indexOf(data[i].product) === -1) {
+                products.push(data[i].product)
+            }
+        }
+        setOwners(owners)
+        setProducts(products)
+    }
+}
+
+const productsTemplate = () => {
+    // TODO: multiselect for products filter list
+}
+
+const ownersTemplate = () => {
+    // TODO: multiselect for owners filter list
 }
 
 const checkboxTemplate = () => {
